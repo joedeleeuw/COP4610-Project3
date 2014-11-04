b9e8a326cf4efead0725128789510d160de3837f
Joe Deleeuw <jdeleeuw@fsu.edu>
2014-11-03 22:45:36 -0500

    structs to hold some ints and char bytes from the filesystem

---
+++ Filesystem.h
@@ -0,0 +1,38 @@
+#ifndef FILESYSTEM_H_
+#define FILESYSTEM_H_
+
+
+struct twoInts
+{
+	int firstInt;
+	int secondInt;
+
+}myInts;
+
+struct twoChars
+{
+	char firstChar;
+	char secondChar;
+
+
+}myChars;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
