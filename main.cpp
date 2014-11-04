165ffb5e8f47d19f771f15be4e951fff54983069
Joe Deleeuw <jdeleeuw@fsu.edu>
2014-11-03 22:02:18 -0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <istream>
+#include <fstream>
+#include <cstdlib>
+#include <uniistd.h>
+#include <stdio.h>
+
+void handleCommand(char*[], int);
+
+int main (int argc, char* argv[])
+{
+
+return 0;
+}
+
+void handleCommand(char *argv[], int argc)
+{
+    if (argc == 0)
+    {
+        printf("%s\n","not enoguh arguments supplied");
+        
+    }
+	else if
+}
