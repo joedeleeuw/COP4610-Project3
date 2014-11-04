0806b65e5b1661523bd63741636f5d0d81538b46
Joe Deleeuw <jdeleeuw@fsu.edu>
2014-11-03 22:40:30 -0500

    woo more boiler plate

--- main.cpp
+++ main.cpp
@@ -11,16 +11,104 @@ void handleCommand(char*[], int);
 
 int main (int argc, char* argv[])
 {
-
-return 0;
+	bool input = true;
+	handleCommand(argc, argv);
+	return 0;
 }
 
 void handleCommand(char *argv[], int argc)
 {
+    string userArg = argv[0];
+    
     if (argc == 0)
     {
         printf("%s\n","not enoguh arguments supplied");
         
     }
-	else if
+	else if (userArg.compare("fsinfo"))
+	{
+
+	}
+	else if (userArg.compare("open"))
+	{
+		if (argc != 2)
+		{
+			printf("%s\n", "wrong amount of arguments supplied");
+		}
+	}
+	else if (userArg.compare("close"))
+	{
+		if (argc != 1)
+		{
+			printf("%s\n", "wrong amount of arguments supplied");
+		}
+	}
+	else if (userArg.compare("create"))
+	{
+		if (argc != 1)
+		{
+			printf("%s\n", "wrong amount of arguments supplied");
+		}
+	}
+	else if (userArg.compare("read"))
+	{
+		if (argc != 3)
+		{
+			printf("%s\n", "wrong amount of arguments supplied");
+		}
+	}
+	else if (userArg.compare("write"))
+	{
+		if (argc != 3)
+		{
+			printf("%s\n", "wrong amount of arguments supplied");
+		}
+	}
+	else if (userArg.compare("rm"))
+	{
+		if (argc != 1)
+		{
+			printf("%s\n", "wrong amount of arguments supplied");
+		}
+	}
+	else if (userArg.compare("cd"))
+	{
+		if (argc != 1)
+		{
+			printf("%s\n", "wrong amount of arguments supplied");
+		}
+	}
+	else if (userArg.compare("ls"))
+	{
+		if (argc != 1)
+		{
+			printf("%s\n", "wrong amount of arguments supplied");
+		}
+	}
+	else if (userArg.compare("mkdir"))
+	{
+		if (argc != 1)
+		{
+			printf("%s\n", "wrong amount of arguments supplied");
+		}
+	}
+	else if (userArg.compare("rmdir"))
+	{
+		if (argc != 1)
+		{
+			printf("%s\n", "wrong amount of arguments supplied");
+		}
+	}
+	else if (userArg.compare("size"))
+	{
+		if (argc != 1)
+		{
+			printf("%s\n", "wrong amount of arguments supplied");
+		}
+	}
+	else if (userArg.compare("undelete"))
+	{
+
+	} 
+
 }
